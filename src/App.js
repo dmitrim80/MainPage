5189c0e2766a8147b56beaa95f2d5728829d4225
Dmitri Morozov <114444260+dmitrim80@users.noreply.github.com>
2024-05-02 16:04:42 -0400

    adding icon manifesto issue

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ export default function App() {
       <Routes>
         <Route path="/viewip" element={<ViewIP />} />
         <Route path="/" element={<Main />} />
-        
       </Routes>
     </BrowserRouter>
   );
