5735fd02d88857feb059cb015dddc341439c4eb6
Dmitri Morozov <114444260+dmitrim80@users.noreply.github.com>
2024-06-08 22:15:38 -0400

    updated with links

--- src/Main_Page/SOAeg.js
+++ src/Main_Page/SOAeg.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './soa.css'
+import './archive.css'
 
 const SOAeg = () => {
+  const [spotlightPosition, setSpotlightPosition] = useState({
+    x: -2,
+    y: -2,
+  });
+  useEffect(() => {
+    const updateSpotlightPosition = (e) => {
+      setSpotlightPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", updateSpotlightPosition);
+    return () => {
+      window.removeEventListener("mousemove", updateSpotlightPosition);
+    };
+  }, []);
   return (
+    <>
+    <div className="archive-container">
+    <div
+          className="overlay"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: `radial-gradient(circle 100px at 
+                ${spotlightPosition.x}px 
+                ${spotlightPosition.y}px, 
+                rgba(4, 79, 226,0.09) 0%,
+                rgba(0,0,0,0.2) 700%)`,
+            pointerEvents: "none",
+            zIndex: 9999,
+          }}
+        ></div>
+        <a href="/archive" className="project-link">
+          <span
+            id="project-arrow-archive"
+            role="img"
+            aria-label="Link to project"
+          >
+            ←
+          </span>
+          <span className="project-header-archive">All Projects</span>
+        </a><br/>
+        <a href="/" className="project-link">
+          <span
+            id="project-arrow-archive"
+            role="img"
+            aria-label="Link to project"
+          >
+            ←
+          </span>
+          <span className="project-header-archive">Dmitri Morozov</span>
+        </a>
     <div>
         <p className='container h6 text-left m-4'>
         Implementing a Service-Oriented Architecture (SOA) in my portfolio React app involves creating services that can be consumed by different parts of my application. <br /><br />
@@ -64,7 +117,10 @@ useEffect(() => {
         This example demonstrates how I can apply SOA principles in your React application by creating modular and reusable services.
         </p>
 
+      </div>
     </div>
+    </>
+    
   )
 }
 
